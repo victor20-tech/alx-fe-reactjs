db85af3890b35387a0872ee8f602d9a8b5165455
victor-tech20 <victoradomako10@gmail.com>
2024-08-10 14:01:45 +0000

    Update user profile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 const UserProfile = (props) => {
-  const { name, age, bio } = props;
-
   return (
     <div>
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
