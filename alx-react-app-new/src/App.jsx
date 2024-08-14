e015c0cf7c9b54dfb9f48ee5e6966a4b0923275b
victor-tech20 <victoradomako10@gmail.com>
2024-08-14 16:41:58 +0000

    Add Count.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Couter";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
     </div>
   );
 }
