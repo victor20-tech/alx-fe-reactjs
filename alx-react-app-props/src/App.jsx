12ae7fdd516cc0d047fa643b9e6b6bcacba99400
victor-tech20 <victoradomako10@gmail.com>
2024-08-15 19:14:48 +0000

    Updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfilePage from "./components/ProfilePage";
-import { UserContext } from "./UserContext";
+import { UserContext } from "./Contexts/UserContext";
 
 const App = () => {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
