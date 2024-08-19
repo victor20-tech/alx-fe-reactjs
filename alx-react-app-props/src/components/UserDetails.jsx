c8b2d6a67c8b2297f34893a992fb9658138beacb
victor-tech20 <victoradomako10@gmail.com>
2024-08-19 14:32:02 +0000

    Updating User Context

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserContext } from "../UserContext";
+import { UserContext } from "./UserContext";
 
 const UserDetails = () => {
   const userData = useContext(UserContext);
