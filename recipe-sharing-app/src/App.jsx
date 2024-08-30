5c6f2a1b086bf2a79ef46797d31bbfa2d9f2bb33
victor-tech20 <victoradomako10@gmail.com>
2024-08-30 12:17:16 +0000

    setup the App.jsx to contain the routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>
+          <h1>Recipe Sharing Application</h1>
+          <AddRecipeForm />
+          <RecipeList />
+        </div>} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 };
 
