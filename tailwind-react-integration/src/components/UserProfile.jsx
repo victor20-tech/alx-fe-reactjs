39f03ec3dc7ce04b02f1e7e266402358e8e0f9a6
victor-tech20 <victoradomako10@gmail.com>
2024-09-08 22:07:19 +0000

    Update UserProfile to responsive on mobile screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
     return (
-        <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
+        <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl">
             <img
                 src="https://via.placeholder.com/150"
                 alt="User"
-                className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+                className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
             />
-            <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
+            <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+                John Doe
+            </h1>
             <p className="text-sm sm:text-base md:text-lg text-gray-600">
                 Developer at Example Co. Loves to write code and explore new technologies.
             </p>
