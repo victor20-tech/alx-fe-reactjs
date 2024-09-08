1eadabe68d7fad5fcd9befbd8474e7de30813ae4
victor-tech20 <victoradomako10@gmail.com>
2024-09-08 21:50:25 +0000

    Add user profile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import UserProfile from './components/UserProfile';
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <UserProfile />
   );
 }
 
