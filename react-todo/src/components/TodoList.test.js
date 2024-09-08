48a57ac160fec9fd998d2beda7dc99194dd6254d
victor-tech20 <victoradomako10@gmail.com>
2024-09-08 23:04:55 +0000

    Add the Todolist test file to components

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import TodoList from "../TodoList";
+import TodoList from "./TodoList";
 
 test("renders TodoList component", () => {
   render(<TodoList />);
