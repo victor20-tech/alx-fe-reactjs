0b29daa1f2c70d1e79eeb5a6c0096cf6aebc5cbd
victor-tech20 <victoradomako10@gmail.com>
2024-09-14 00:21:37 +0000

    Add hover effect to img

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
             <img
                 src="https://via.placeholder.com/150"
                 alt="User"
-                className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+                className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
             />
             <h1 className="text-xl text-blue-800 my-4">
                 John Doe
