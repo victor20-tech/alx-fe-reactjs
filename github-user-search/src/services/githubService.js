a4dc91f671d7e5009d52d793bc7991403a1b6498
victor-tech20 <victoradomako10@gmail.com>
2024-09-22 17:15:35 +0000

    Fix errors in the githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com/search/users";
+const GITHUB_API_URL = "https://api.github.com/search/users?q";
 
 export const fetchUserData = async (username, location, minRepos) => {
   try {
