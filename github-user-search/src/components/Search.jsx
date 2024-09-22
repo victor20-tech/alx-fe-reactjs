fb8a1311f4cd688250ed1200c2ac306f3120e237
victor-tech20 <victoradomako10@gmail.com>
2024-09-22 17:04:53 +0000

    Fix errors in the Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -43,7 +43,7 @@ function Search() {
             </form>
 
             {loading && <p>Loading...</p>}
-            {error && <p>Looks like we can't find the user.</p>}
+            {error && <p>Looks like we cant find the user</p>}
             {userData && (
                 <div>
                     <img src={userData.avatar_url} alt={userData.name} width="100" />
