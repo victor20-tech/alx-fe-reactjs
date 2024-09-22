68363869fc8e00ceb06507aca30d9ba660972353
victor-tech20 <victoradomako10@gmail.com>
2024-09-22 16:55:35 +0000

    Fix errors in the githubService.js

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   const handleSearch = async (username) => {
     setLoading(true);
     setError(false);
-    setUserData(null); // Reset the user data
+    setUserData(null); // Reset user data
 
     try {
       const data = await fetchUserData(username);
       setUserData(data); // Set the fetched user data
     } catch (err) {
-      setError(true); // Set error if user is not found
+      setError(true); // Set error if user not found
     } finally {
       setLoading(false);
     }
@@ -25,20 +25,12 @@ function App() {
   return (
     <div>
       <h1>GitHub User Search Application</h1>
-      <Search onSearch={handleSearch} />
-
-      {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
-      {userData && (
-        <div>
-          <img src={userData.avatar_url} alt={userData.name} width="100" />
-          <h2>{userData.name}</h2>
-          <p>Username: {userData.login}</p>
-          <a href={userData.html_url} target="_blank" rel="noreferrer">
-            Visit GitHub Profile
-          </a>
-        </div>
-      )}
+      <Search
+        onSearch={handleSearch}
+        userData={userData}
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 }
